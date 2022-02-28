aea3e570852c6658358e69fe35dd0afc47899614
Balkrishan Sehra <42343273+Renon133@users.noreply.github.com>
2022-02-28 13:52:21 +0530

    minor changes

--- appointmentsystem/src/main/java/com/team7/appointmentsystem/services/ProfileService.java
+++ appointmentsystem/src/main/java/com/team7/appointmentsystem/services/ProfileService.java
@@ -1,6 +1,7 @@
 package com.team7.appointmentsystem.services;
 
 import com.team7.appointmentsystem.entity.Users;
+import com.team7.appointmentsystem.exceptions.PasswordsDoNotMatchException;
 import com.team7.appointmentsystem.models.PasswordObject;
 import com.team7.appointmentsystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +20,11 @@ public class ProfileService {
     public PasswordObject changePassword(String oldPassword, String newPassword, Long userId) {
 
         Users userOptional = userRepo.findByUserid(userId);
+        PasswordObject dummy = new PasswordObject();
         try {
             if (userOptional != null) {
                 Users user = userOptional;
-                PasswordObject dummy = new PasswordObject();
+
                 System.out.println(oldPassword + ": is the raw password");
                 if (passwordEncoder.matches(oldPassword, user.getUserPassword())) {
                     dummy.setOldPassword(user.getUserPassword());
@@ -30,17 +32,17 @@ public class ProfileService {
                     dummy.setNewPassword(user.getUserPassword());
                     System.out.println(newPassword + ": Password Changed Successfully");
                     userRepo.save(user);
-                    return dummy;
+
                 } else {
-                    System.out.println("Password do not match");
+                    throw new PasswordsDoNotMatchException("Password do not match");
                 }
             }
         } catch (Exception e) {
 //            throw new Exception("Couldn't Change Password");
             e.printStackTrace();
-
+        }finally {
+            return dummy;
         }
-        return null;
     }
 
     public Users getUser(long UserId) {
